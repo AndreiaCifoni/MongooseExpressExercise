0f9c97f342dcfb3129d608b67cbe66e27c806e91
Andreia Cifoni <andreiacifoni@gmail.com>
2022-02-27 15:51:25 -0500

    new product added

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ mongoose
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
@@ -30,10 +31,15 @@ app.get("/products/new", (req, res) => {
   res.render("products/new");
 });
 
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  console.log(product);
   res.render("products/show", { product });
 });
 
